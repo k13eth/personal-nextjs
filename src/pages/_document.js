a5057b3875821f8d74bc82251b3e827ebd636ff0
Artem Denysov <denysov.artem@gmail.com>
2021-08-04 17:28:53 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { Helmet } from 'react-helmet';
-import { withPrefix } from '../utils';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
